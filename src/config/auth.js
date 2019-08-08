8034a5d65d2ea94afc8fcc8c353323dce70d3fc0
Renan Soares <rjrenan77@gmail.com>
2019-08-07 21:22:50 -0300

    autenticacao com jwt, validacao de dados, cadastro e atualizacao de usuários, bcrypt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b44038a4f486483699d99a1e82f95888',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Renan SOares',
-    email: 'renan@renan',
-    password_hash: '2345',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
